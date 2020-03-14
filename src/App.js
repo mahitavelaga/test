dc156842f01014590b04f2abe7f5a195307925e5
Mahi Velaga <mahitavelaga@gmail.com>
2020-03-14 11:24:19 -0500

    im a react world expert 2.0

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div className="navbar">
+        
+        <div className="left">
+          HOME ABOUT
+        </div>
+        
+        <div className="right">
+          MAHI THE POOP
+        </div>
+
+      </div>
+
+      <div className="body">
+        i am a secnd divasdasdasd
+      </div>
+    </div>
+  );
+}
+
+export default App;
